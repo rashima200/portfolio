2ee6337da432d2c11618401625eb687f4b937081
rashima200 <rashimarasangi200@gmail.com>
2025-03-12 21:45:20 +0530

    feat:contact section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Service from './components/Service'
+import Project from './components/Project'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -10,6 +14,10 @@ const App = () => {
      <Navbar/> 
      <Hero/>
      <About/>
+     <Service/>
+     <Project/>
+     <Contact/>
+     <Footer/>
     </div>
   )
 }
